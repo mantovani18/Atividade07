7242bd209ed6f4f200a3cf43b164d653650e4df9
Pedro Henrique Mantovani <128389480+mantovani18@users.noreply.github.com>
2026-03-16 20:27:19 -0300

    adicionado_variavel_No_anterior

--- ATV07/No.java
+++ ATV07/No.java
@@ -1,9 +1,11 @@
 public class No{
     String nome;
     No proximo;
+    No anterior;
 
     public No(String nome){
         this.nome = nome;
         this.proximo = null;
+        this.anterior = null;
     }
 }
